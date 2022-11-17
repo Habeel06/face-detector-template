671970ec9cec1825533b8604bc80f333b3d72d92
Mir Habeel Ahmad <102403483+Habeel06@users.noreply.github.com>
2022-11-17 11:18:10 +0530

    Add files via upload

---
+++ faces.py
@@ -0,0 +1,60 @@
+import numpy as np
+import cv2
+import pickle
+
+
+
+face_cascade = cv2.CascadeClassifier('C:/Users/HP/Desktop/MAIN/Scripts/PROJECTS/FaceDetector/cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('C:/Users/HP/Desktop/MAIN/Scripts/PROJECTS/FaceDetector/trainer.yml')
+
+labels={'person_name':1}
+with open("C:/Users/HP/Desktop/MAIN/Scripts/PROJECTS/FaceDetector/labels.pickle",'rb') as f:#save ids
+    op_labels=pickle.load(f)
+    labels={v:k for k,v in op_labels.items()}
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret ,frame = cap.read()#capturing fram by frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#grayscale
+    faces = face_cascade.detectMultiScale(gray,scaleFactor=1.5 ,minNeighbors=5)
+    for(x,y,w,h) in faces:
+        # print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]#region of interest
+        roi_color = frame[y:y+h, x:x+w]
+
+
+        #recognize?
+        id_, conf=recognizer.predict(roi_gray)
+        if conf>=45 and conf <=85:#conf-confidence
+            # print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name=labels[id_]
+            color =(255,255,255)
+            stroke=1
+            cv2.putText(frame,name,(x,y),font,1,color,stroke,cv2.LINE_AA)
+
+
+
+        
+        img_item = 'my-image.png'
+        cv2.imwrite(img_item,roi_gray)
+        
+        end_cord_x=x+w #box
+        end_cord_y=-y+h
+        color = (225,0,0)
+        stroke = 2
+        cv2.rectangle(frame, (x,y),(end_cord_x,end_cord_y),color ,stroke)
+ 
+    cv2.imshow('frame', frame)#displaying video
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+     break
+
+
+
+
+cap.release()
+cv2.destroyAllWindows()   
+   
---
+++ faces.train.py
@@ -0,0 +1,60 @@
+from cProfile import label
+from PIL import Image
+import os
+import numpy as np
+import cv2
+import pickle
+
+
+
+
+face_cascade = cv2.CascadeClassifier('C:/Users/HP/Desktop/MAIN/Scripts/PROJECTS/FaceDetector/cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+
+
+BASE_DIR =os.path.dirname(os.path.abspath(__file__))
+image_dir= os.path.join(BASE_DIR, "images")
+current_id=0
+label_ids={}
+y_labels=[]
+x_train=[] 
+
+for root, dirs ,files in os.walk(image_dir):
+    for file in files:
+        if file.endswith('png') or file.endswith("jpg"):
+            path = os.path.join(root,file)
+            label=os.path.basename(os.path.dirname(path)).replace(" ","-").lower()
+            # print(label,path)
+
+
+            if not label in label_ids:
+                label_ids[label]=current_id
+                current_id+=1
+            
+            id_= label_ids[label]
+            # print(label_ids)
+            # y_labels.append(label) #some no.
+            # x_train.append(path)  #verify this image and then turn it into NumPy array ,GRAY
+            pill_image= Image.open(path).convert("L")#into grayscale
+            image_array =np.array(pill_image,"uint8")#into nunpy array
+            # print(image_array)
+            faces = face_cascade.detectMultiScale(image_array,scaleFactor=1.5 ,minNeighbors=5)
+            
+            for (x,y,w,h) in faces:
+                roi =image_array[y:y+h , x:x+w]
+                x_train.append(roi)#arrays
+                y_labels.append(id_)#y labels into numpy arrays
+
+
+ 
+# print(y_labels)
+# print(x_train)
+
+
+with open("labels.pickle",'wb') as f:#save ids
+     pickle.dump(label_ids,f)
+
+
+recognizer.train(x_train,np.array(y_labels))
+recognizer.save("trainer.yml")
